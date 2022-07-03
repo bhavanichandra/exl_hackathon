6e1a62e69e4a17405ff9ced8dbbd44b3f73619f0
bhavanichandra <bhavanichandra9@gmail.com>
2022-07-03 21:58:37 +0530

    Removed mongodb

--- fileservice/src/main/java/com/themuler/fs/FileServiceApplication.java
+++ fileservice/src/main/java/com/themuler/fs/FileServiceApplication.java
@@ -2,8 +2,10 @@ package com.themuler.fs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class FileServiceApplication {
 
   public static void main(String[] args) {
@@ -11,3 +13,5 @@ public class FileServiceApplication {
   }
 
 }
+
+
